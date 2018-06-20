8a518de877d545da9be6ea5eb51d12659e6af487
Luca <gallucciluca94@gmail.com>
2018-06-20 11:03:27 +0200

    manage 2 players

--- src/it/unicam/cs/pa/lg/forza4/net/Connect4Server.java
+++ src/it/unicam/cs/pa/lg/forza4/net/Connect4Server.java
@@ -2,13 +2,15 @@ package it.unicam.cs.pa.lg.forza4.net;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Connect4Server
 {
 	public final static int PORT = 9001;
-	private ExecutorService executor = Executors.newCachedThreadPool();
+	public final static int MAX_NUM_PLAYER = 2;
+	private int playerCounter = 0;
 	
 	public void start()
 	{
@@ -16,13 +18,35 @@ public class Connect4Server
 		{
 			ServerSocket server = new ServerSocket(PORT);
 			System.out.println("Accept connections...");
-			server.accept();
-			System.out.println("OK!");
-
+			
+			while (playerCounter != MAX_NUM_PLAYER)
+			{
+				Socket client = server.accept();
+				playerCounter++;
+				System.out.println("Player " + playerCounter + " connected");
+				
+				new Thread(new SessionThread(client)).start();
+			}
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
+		}
+	}
+	
+	private class SessionThread implements Runnable
+	{
+		private Socket client = null;
+
+		public SessionThread(Socket client)
+		{
+			this.client = client;
+		}
+
+		public void run()
+		{
+			System.out.println("Connected with " + client.getInetAddress().toString());
+//			new C4ServerSession(clientSocket);
 		}
 	}
 }
