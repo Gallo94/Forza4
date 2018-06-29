fe629565c0830f9844659f0a0afe138a8d0054e0
Luca <gallucciluca94@gmail.com>
2018-06-29 12:01:58 +0200

    improve PlayerAI

--- src/it/unicam/cs/pa/lg/forza4/PlayerAI.java
+++ src/it/unicam/cs/pa/lg/forza4/PlayerAI.java
@@ -9,6 +9,10 @@ public class PlayerAI extends Player
 		
 	private int[] valueColumn = {3, 4, 2, 5, 1, 6, 0};
 	
+	public static final int MUST_WIN = 3; 
+	public static final int MUST_BLOCK = 2;
+	public static final int NORMAL = 1; 
+	
 	public PlayerAI(int id)
 	{
 		super(id);
@@ -20,27 +24,37 @@ public class PlayerAI extends Player
 	{
 		// Get available positions
 		ArrayList<Integer> availableColumns = grid.getAvailableColumns();
-		for (int i = 0; i < availableColumns.size(); i++)
-		{
-			System.out.println(availableColumns.get(i) + " : " + grid.getAvailColumnPositions(availableColumns.get(i)));
-		}
 		
 		// Evaluate weights
 		int opponentId = 0;
 		int[] columnValues = new int[availableColumns.size()];
 		for (int i = 0; i < columnValues.length; i++)
 		{
-			int col = availableColumns.get(i); 
+			int col = availableColumns.get(i);
+			boolean mustWin = grid.checkWin(this.id, grid.getAvailRowIndex(col), col);
 			boolean mustBlock = grid.checkWin(opponentId, grid.getAvailRowIndex(col), col);
-			columnValues[i] = mustBlock ? 2 : 1;
+			columnValues[i] = mustWin   ? MUST_WIN : 
+							  mustBlock ? MUST_BLOCK : NORMAL; 
 		}
 		
 		ArrayList<Integer> bestColumns = new ArrayList<>();
+
 		for (int i = 0; i < columnValues.length; i++)
 		{
-			if (columnValues[i] == 2)
+			if (columnValues[i] == MUST_WIN)
 			{
 				bestColumns.add(availableColumns.get(i));
+			}
+		}
+
+		if (bestColumns.size() == 0)
+		{
+			for (int i = 0; i < columnValues.length; i++)
+			{
+				if (columnValues[i] == MUST_BLOCK)
+				{
+					bestColumns.add(availableColumns.get(i));
+				}
 			}
 		}
 		
@@ -48,14 +62,24 @@ public class PlayerAI extends Player
 		{
 			for (int i = 0; i < columnValues.length; i++)
 			{
-				if (columnValues[i] == 1)
+				if (columnValues[i] == NORMAL)
 				{
 					bestColumns.add(availableColumns.get(i));
 				}
 			}			
 		}
-			
+		
 		int index = (int) (Math.random() * bestColumns.size());
+
+		// Debug
+		System.out.println("Available columns");
+		for (int i = 0; i < availableColumns.size(); i++)
+		{
+			System.out.println(availableColumns.get(i) + " : " + grid.getAvailColumnPositions(availableColumns.get(i)));
+		}
+		
+		System.out.println("bestColumns[" + index + "] = " + bestColumns.get(index));
+					
 		return bestColumns.get(index);
 	}
 }
